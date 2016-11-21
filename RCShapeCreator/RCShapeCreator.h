0a52df0d64618f9f2e8d8a0b7e0918518378650f
Ryan Cocuzzo <rcocuzzo8@gmail.com>
2016-11-21 17:06:30 -0500

    Initial Commit

---
+++ RCShapeCreator/RCShapeCreator.h
@@ -0,0 +1,19 @@
+//
+//  RCShapeCreator.h
+//  RCShapeCreator
+//
+//  Created by Ryan Cocuzzo on 11/21/16.
+//  Copyright © 2016 rcocuzzo8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RCShapeCreator.
+FOUNDATION_EXPORT double RCShapeCreatorVersionNumber;
+
+//! Project version string for RCShapeCreator.
+FOUNDATION_EXPORT const unsigned char RCShapeCreatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCShapeCreator/PublicHeader.h>
+
+
